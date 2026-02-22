3c9ef75b0a04b9885978946b9866390369e1f0c4
Veronika050987 <veronicaviv@mail.ru>
2026-02-22 13:30:45 +0300

    Unique key and page nimbers style DONE

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,14 @@
 import './MovieList.css';
 import Movie from './Movie.js';
 
-function MovieList(props)
-{
+function MovieList(props){
     const { movies = [] } = props;
+    const unique = Array.from(new Map(movies.map(m=>[m.imdbID, m])).values());
     return(
         <div className='movies'>
             {
-                movies.length ? movies.map
-                (
-                    
+                unique.length ? unique.map
+                (                   
                     movie => 
                     {
                         return <Movie key={movie.imdbID} {...movie}/>
